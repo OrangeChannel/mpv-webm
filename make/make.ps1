495428cde8b9fb60fa22db0437a73ad8398b657d
Dave <orangechannel@pm.me>
2019-08-01 02:18:38 +0000

    formats: move format base file out of folder
    This makes the build task a bit cleaner, and prevents forgetting to add a format file to the build.

--- make/make.ps1
+++ make/make.ps1
@@ -3,7 +3,7 @@ Set-Location ./src
 mkdir ../temp
 Get-Content requires.lua, options.lua | Set-Content ../temp/header.lua
 #grab moon files
-Get-Content util.moon, video_to_screen.moon, formats/base.moon, formats/rawvideo.moon, formats/webm.moon, formats/mp4.moon, Page.moon, EncodeWithProgress.moon, encode.moon, CropPage.moon, EncodeOptionsPage.moon, PreviewPage.moon, MainPage.moon, main.moon | Set-Content ../temp/main.moon
+Get-Content util.moon, video_to_screen.moon, formatsBase.moon, formats/*.moon, Page.moon, EncodeWithProgress.moon, encode.moon, CropPage.moon, EncodeOptionsPage.moon, PreviewPage.moon, MainPage.moon, main.moon | Set-Content ../temp/main.moon
 moonc -o ../temp/main.lua ../temp/main.moon
 Get-Content ../temp/header.lua, ../temp/main.lua | Set-Content ../build/webm.lua
 Remove-Item ../temp -Recurse
